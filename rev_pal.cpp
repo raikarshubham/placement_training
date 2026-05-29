f3642c27a90aa3e1bd061063f4326b7e2e1a776c
raikarshubham <raikarshubhamvaikunt@gmail.com>
2026-05-30 01:01:45 +0530

    day_one

---
+++ rev_pal.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int rev=0;
+    int num=121;
+    int temp=num;
+    while(num>0){
+        int digit=num % 10;
+        rev=(rev*10)+digit;
+        num=num/10;
+    }
+    cout<<rev<<endl;
+    if(temp==rev){
+        cout<<"The Number "<<temp<< " is Palindrome";
+    }
+    else{
+        cout<<"The Number "<<temp<< " is not Palindrome";
+    }
+    return 0;
+}
